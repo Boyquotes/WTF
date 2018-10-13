7bb2e1c5c6c3990079ad0e18d51ddfd94cf4f733
root <root@linux-1.home>
2018-10-13 17:10:33 +0000

    Add tag entity

---
+++ src/WTFBundle/Entity/Tag.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace WTFBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Tag
+ *
+ * @ORM\Table(name="tag")
+ * @ORM\Entity(repositoryClass="WTFBundle\Repository\TagRepository")
+ */
+class Tag
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="cat", type="integer")
+     */
+    private $cat;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     *
+     * @return Tag
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set cat
+     *
+     * @param integer $cat
+     *
+     * @return Tag
+     */
+    public function setCat($cat)
+    {
+        $this->cat = $cat;
+
+        return $this;
+    }
+
+    /**
+     * Get cat
+     *
+     * @return int
+     */
+    public function getCat()
+    {
+        return $this->cat;
+    }
+}
+
